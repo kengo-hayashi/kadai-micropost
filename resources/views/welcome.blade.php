4534be4cbd549748d3cad34e660ddb138cb75f1c
kengo.hayashi <kengo.hayashi@rakuten.com>
2018-06-04 03:08:50 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,10 +77,16 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            @extends('layouts.app')
+
+            @section('content')
+                <div class="center jumbotron">
+                    <div class="text-center">
+                        <h1>Welcome to the Microposts</h1>
+                        {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+                    </div>
                 </div>
+            @endsection
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
