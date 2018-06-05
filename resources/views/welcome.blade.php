0ab136e186bd79af8b50d1056ae200710a77e475
kengo.hayashi <kengo.hayashi@rakuten.com>
2018-06-05 01:36:45 +0000

    post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,28 +65,29 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
             @extends('layouts.app')
 
             @section('content')
-                <div class="center jumbotron">
-                    <div class="text-center">
-                        <h1>Welcome to the Microposts</h1>
-                        {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+                @if (Auth::check())
+                    <div class="row">
+                        <aside class="col-md-4">
+                        </aside>
+                        <div class="col-xs-8">
+                            @if (count($microposts) > 0)
+                                @include('microposts.microposts', ['microposts' => $microposts])
+                            @endif
+                        </div>
                     </div>
-                </div>
-            @endsection
+            @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
+@endsection
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
-// user registration
-Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
-Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+// omit
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+});
